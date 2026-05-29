297a1412ba72dee1f91f752e910739d819c38085
Ncristofe <cristofegit@gmail.com>
2026-05-29 07:42:39 -0300

    feat: add LoginResponseDTO to return token and user profile details

--- src/main/java/almoxarifado_backend/dto/LoginResponseDTO.java
+++ src/main/java/almoxarifado_backend/dto/LoginResponseDTO.java
@@ -1,4 +1,14 @@
 package almoxarifado_backend.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class LoginResponseDTO {
+    private String token;
+    private String tipo;
+    private String nome;
+    private String email;
+    private Long id;
 }
