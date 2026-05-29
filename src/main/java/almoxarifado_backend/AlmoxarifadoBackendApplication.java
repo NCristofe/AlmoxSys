6fa2d87ce4ae839936f274c716e6abbaf3de1f36
Ncristofe <cristofegit@gmail.com>
2026-05-29 06:49:10 -0300

    feat: estrutura inicial spring boot

---
+++ src/main/java/almoxarifado_backend/AlmoxarifadoBackendApplication.java
@@ -0,0 +1,13 @@
+package almoxarifado_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlmoxarifadoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlmoxarifadoBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/almoxarifado_backend/AlmoxarifadoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package almoxarifado_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlmoxarifadoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
